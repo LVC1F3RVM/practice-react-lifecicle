23520bd0dbf354781245772d6eec47d5dd9e4e75
Name <username@gmail.com>
2020-07-30 22:40:57 +0300

    React App and Axios downloaded

---
+++ practice-react-lifecicle/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import axios from "axios";
+
+import User from "./components/User";
+
+const api = "https://5f22f8cf0e9f660016d88b17.mockapi.io/";
+const usersAPI = `${api}users`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      warning: "",
+    };
+  }
+
+  componentDidMount() {
+    axios.get(usersAPI).then(({ data }) => {
+      this.setState({ users: data });
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.state.users.length < 6 && this.state.users.length !== 0) {
+      this.setState({ warning: "You have deleted a lot of users!!!" });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(this.state, nextState);
+    if (this.state.users.length === nextState.users.length) {
+      return false;
+    }
+    return true;
+  }
+
+  handleClick = (e) => {
+    const { users } = this.state;
+    const newUsers = [...users];
+    newUsers.pop();
+    this.setState({ users: newUsers });
+  };
+
+  render() {
+    const { users, warning } = this.state;
+    return (
+      <>
+        <button onClick={this.handleClick}>Delete last user</button>
+        <div>Users count: {users.length}</div>
+        {!!warning && <p className="title">{warning}</p>}
+
+        <ul>
+          {users.length > 0
+            ? users.map((user) => (
+                <User key={user.id} name={user.name} avatar={user.avatar} />
+              ))
+            : "no users found"}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default App;
